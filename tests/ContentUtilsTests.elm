b983d6a71050def6ac64e2010e18d6e2c250ae0a
Gabriel Perales <gabrielperales@gmail.com>
2017-03-10 18:59:30 +0100

    added tests for filterByContentType and for flipComparison

--- tests/ContentUtilsTests.elm
+++ tests/ContentUtilsTests.elm
@@ -39,49 +39,21 @@ all =
         [ describe "findBySlug"
             [ test "Empty content" <|
                 \() ->
-                    let
-                        content =
-                            []
-
-                        expected =
-                            Nothing
-
-                        actual =
-                            findBySlug content "whatever"
-                    in
-                        Expect.equal expected actual
+                    Expect.equal Nothing <| findBySlug [] "whatever"
             , test "Just one element" <|
                 \() ->
                     let
                         post =
                             { mockContent | slug = "slug" }
-
-                        content =
-                            [ post ]
-
-                        expected =
-                            Just post
-
-                        actual =
-                            findBySlug content "slug"
                     in
-                        Expect.equal expected actual
+                        Expect.equal (Just post) <| findBySlug [ post ] "slug"
             , test "Slug not in the content" <|
                 \() ->
                     let
                         post =
                             { mockContent | slug = "slug" }
-
-                        content =
-                            [ post ]
-
-                        expected =
-                            Nothing
-
-                        actual =
-                            findBySlug content "not-in-content"
                     in
-                        Expect.equal expected actual
+                        Expect.equal Nothing <| findBySlug [ post ] "not-in-content"
             , test "Slug in the middle of the content" <|
                 \() ->
                     let
@@ -90,13 +62,45 @@ all =
 
                         content =
                             [ p, p, p, p, { p | slug = "slug" }, p ]
-
-                        expected =
-                            Just { p | slug = "slug" }
-
-                        actual =
-                            findBySlug content "slug"
                     in
-                        Expect.equal expected actual
+                        Expect.equal (Just { p | slug = "slug" }) <| findBySlug content "slug"
+            ]
+        , describe "filterByContentType" <|
+            -- I have the feeling that I've been testing the filter function, and I shouldn't
+            [ test "Empty content" <|
+                \() ->
+                    Expect.equal 0 <| List.length <| filterByContentType Page []
+            , test "No content of that ContentType" <|
+                \() ->
+                    let
+                        content =
+                            [ { mockContent | contentType = Post }, { mockContent | contentType = Post } ]
+                    in
+                        Expect.equal 0 <| List.length <| filterByContentType Page content
+            , test "Just one content of that ContentType" <|
+                \() ->
+                    let
+                        content =
+                            [ { mockContent | contentType = Page }, { mockContent | contentType = Post }, { mockContent | contentType = Post } ]
+                    in
+                        Expect.equal 1 <| List.length <| filterByContentType Page content
+            , test "Two content of that ContentType" <|
+                \() ->
+                    let
+                        content =
+                            [ { mockContent | contentType = Page }, { mockContent | contentType = Post }, { mockContent | contentType = Post } ]
+                    in
+                        Expect.equal 2 <| List.length <| filterByContentType Post content
+            ]
+        , describe "flipComparison" <|
+            [ test "flipComparison of compare 1 2 should be GT" <|
+                \() ->
+                    Expect.equal GT <| flipComparison compare 1 2
+            , test "flipComparison of compare 2 1 should be LT" <|
+                \() ->
+                    Expect.equal LT <| flipComparison compare 2 1
+            , test "flipComparison of compare 1 1 should be EQ" <|
+                \() ->
+                    Expect.equal EQ <| flipComparison compare 1 1
             ]
         ]
